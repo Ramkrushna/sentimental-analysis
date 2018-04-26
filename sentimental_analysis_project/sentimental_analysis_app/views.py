5d402d64f575133c932faa26de3f0e10dc86a703
Ameya Joshi <ameyajoshi99@gmail.com>
2018-04-27 02:08:14 +0530

    Integrated D3.js

--- sentimental_analysis_project/sentimental_analysis_app/views.py
+++ sentimental_analysis_project/sentimental_analysis_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,HttpResponse
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sentimental_analysis_app.utils.data_reader import read_csv_file, get_sentiment_intensity_analyzer
+import json
+#import numpy as np # linear algebra
+#import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+#from sentimental_analysis_app.utils.data_reader import read_csv_file, get_sentiment_intensity_analyzer
 from django.shortcuts import redirect
 from django.conf import settings
 
@@ -15,8 +16,8 @@ def profile(request):
     #return HttpResponse(get_sentiment_intensity_analyzer(read_csv_file()))
 
 def show_bar_chart(request):
-	tweets = get_sentiment_intensity_analyzer(read_csv_file())
-	bar_chart = tweets.sentiment_type.value_counts().plot(kind='bar',title="sentiment analysis")
-	return HttpResponse("Showinng charts...")
+    test_list_dict = [{"label": "Recommended", "value": 60}, {"label": " You", "value": 60}, {"label": "Peers", "value": 40 }]
+
+    return HttpResponse(content=json.dumps(test_list_dict), content_type="application/json")
 
 
